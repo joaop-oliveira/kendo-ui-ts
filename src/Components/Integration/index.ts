cf0c4bc921b7b9d0e44325719bf8ae4bcd35ea9f
João Paulo Oliveira <jpaulo2212gt@gmail.com>
2019-03-03 19:41:13 -0300

    testing typescript is not taht easy

--- src/Components/Integration/index.ts
+++ src/Components/Integration/index.ts
@@ -1,1 +1,1 @@
-export { default as NavBar } from './NavBar/index';
+export { default as Navbar } from "./Navbar";
---
+++ src/Pages/TeamsGrid/typedef.d.ts
@@ -0,0 +1,8 @@
+export interface TeamsGridProps {
+  path: string;
+  //   chidlren: React.ReactNode;
+}
+
+export class TeamsGridState {
+  data: Array<object> = [{}];
+}
